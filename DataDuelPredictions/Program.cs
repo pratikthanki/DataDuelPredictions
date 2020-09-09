c89d97ac4d29fe8ad158c6fd9f195a748813541e
Pratik Thanki <pratikthanki1@gmail.com>
2020-09-10 00:02:53 +0100

    parse games to predict

--- DataDuelPredictions/Program.cs
+++ DataDuelPredictions/Program.cs
@@ -1,12 +1,42 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DataDuelPredictions
 {
     internal static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var model = new Model();
+            var Pending = "Pending";
+            // var model = new Model();
+            var games = await PredictionWriter.GetFixtures();
 
+
+            var matches = games
+                .SelectMany(week => week.fixtures.Where(game => game.status == Pending))
+                .Select(match => new[]
+                    {
+                        new Match
+                        {
+                            MatchDate = match.date,
+                            Team = match.homeTeam.name,
+                            Opponent = match.awayTeam.name,
+                            FullTimeGoals = (float) 0,
+                            IsHome = (float) 1
+                        },
+                        new Match
+                        {
+                            MatchDate = match.date,
+                            Team = match.awayTeam.name,
+                            Opponent = match.homeTeam.name,
+                            FullTimeGoals = (float) 0,
+                            IsHome = (float) 0
+                        }
+                    }
+                ).SelectMany(o => o)
+                .ToList();
         }
     }
 }
