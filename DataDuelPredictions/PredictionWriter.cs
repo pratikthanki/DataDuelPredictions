3392127802a03ecc1a0882b2cdb9e935325d4b12
Pratik Thanki <pratikthanki1@gmail.com>
2020-09-10 11:48:36 +0100

    Update PUT method

--- DataDuelPredictions/PredictionWriter.cs
+++ DataDuelPredictions/PredictionWriter.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
-using System.Runtime.Serialization;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -13,19 +14,23 @@ namespace DataDuelPredictions
         private const string BaseUrl = Settings.BaseUrl;
         private const string ApiKey = Settings.ApiKey;
         private const string ApplicationJson = "application/json";
+        private const string Put = "PUT";
 
         private const int Season = 2020;
-        private static readonly string MatchDayEndpoint = $"/api/season/{Season}/matchday/1/fixture";
-        private static readonly string FixturesEndpoint = $"/api/season/{Season}/fixture";
+        private static readonly string SeasonFixturesEndpoint = $"/api/season/{Season}/fixture";
         private const string PredictionEndpoint = @"/api/prediction";
 
         public PredictionWriter()
         {
         }
 
-        public static async Task<IList<DataDuelMatch>> GetFixtures()
+        public static async Task<IList<DataDuelMatch>> GetFixtures(int? matchday)
         {
-            var request = WebRequest.Create($"{BaseUrl}{FixturesEndpoint}");
+            var endpoint = SeasonFixturesEndpoint;
+            if (matchday != null) 
+                endpoint = $"/api/season/{Season}/matchday/{matchday}/fixture";
+            
+            var request = WebRequest.Create($"{BaseUrl}{endpoint}");
 
             using var response = (HttpWebResponse) await request.GetResponseAsync();
             await using var dataStream = response.GetResponseStream();
@@ -37,22 +42,38 @@ namespace DataDuelPredictions
             return responseJson;
         }
 
-        public static async Task<HttpStatusCode> PutPredictions(List<ScorePrediction> predictions)
+        public static async Task PutPredictions(List<DataDuelPrediction> predictions)
         {
-            var request = WebRequest.Create($"{BaseUrl}{PredictionEndpoint}");
+            var request = (HttpWebRequest) WebRequest.Create($"{BaseUrl}{PredictionEndpoint}");
+            var jsonString = JsonConvert.SerializeObject(predictions);
 
-            request.Method = "PUT";
+            request.Method = Put;
             request.ContentType = ApplicationJson;
             request.Headers.Add(HttpRequestHeader.Authorization, ApiKey);
 
             Debug.Assert(predictions.Count > 0);
 
             await using var dataStream = await request.GetRequestStreamAsync();
-            var ser = new DataContractSerializer(predictions.GetType());
-            ser.WriteObject(dataStream, predictions);
+            var byteArray = Encoding.UTF8.GetBytes(jsonString);
+            var byteArrayLength = byteArray.Length;
+
+            request.ContentLength = byteArrayLength;
+
+            try
+            {
+                await dataStream.WriteAsync(byteArray, 0, byteArrayLength);
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine($"WebException: {e.Status} - {e.Message}");
+            }
 
             var response = (HttpWebResponse) await request.GetResponseAsync();
-            return response.StatusCode;
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("Error in updating DataDuel predictions");
+
+            Console.WriteLine("DataDuel predictions updated");
         }
     }
 }
--- DataDuelPredictions/Program.cs
+++ DataDuelPredictions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,17 +10,19 @@ namespace DataDuelPredictions
     {
         static async Task Main(string[] args)
         {
-            var Pending = "Pending";
-            // var model = new Model();
-            var games = await PredictionWriter.GetFixtures();
+            // Set to null to get and predict all matchday's
+            int? matchday = null;
+            var matchdayFixtures = await PredictionWriter.GetFixtures(matchday);
 
-
-            var matches = games
+            // Only care about matches yet to be played  
+            const string Pending = "Pending";
+            var matches = matchdayFixtures
                 .SelectMany(week => week.fixtures.Where(game => game.status == Pending))
                 .Select(match => new[]
                     {
                         new Match
                         {
+                            FixtureId = match.id,
                             MatchDate = match.date,
                             Team = match.homeTeam.name,
                             Opponent = match.awayTeam.name,
@@ -28,6 +31,7 @@ namespace DataDuelPredictions
                         },
                         new Match
                         {
+                            FixtureId = match.id,
                             MatchDate = match.date,
                             Team = match.awayTeam.name,
                             Opponent = match.homeTeam.name,
@@ -35,8 +39,42 @@ namespace DataDuelPredictions
                             IsHome = (float) 0
                         }
                     }
-                ).SelectMany(o => o)
-                .ToList();
+                ).ToList();
+
+            var predictions = PredictScores(matches);
+            await PredictionWriter.PutPredictions(predictions);
+        }
+
+        private static List<DataDuelPrediction> PredictScores(IEnumerable<Match[]> matches)
+        {
+            var modelBuilder = new ModelBuilder();
+            var trainedModel = modelBuilder.BuildModel();
+
+            var predictions = new List<DataDuelPrediction>();
+
+            foreach (var match in matches)
+            {
+                var homeTeam = match.First(x => x.IsHome == (float) 1);
+                var awayTeam = match.First(x => x.IsHome == (float) 0);
+
+                Debug.Assert(homeTeam.FixtureId == awayTeam.FixtureId);
+
+                var prediction = new DataDuelPrediction
+                {
+                    fixtureId = homeTeam.FixtureId,
+                    score = new PredictedScore
+                    {
+                        homeGoals = modelBuilder.PredictionMatchScore(trainedModel, homeTeam),
+                        awayGoals = modelBuilder.PredictionMatchScore(trainedModel, awayTeam)
+                    }
+                };
+
+                predictions.Add(prediction);
+                Console.WriteLine(
+                    $"{homeTeam.Team} ({prediction.score.homeGoals}) : {awayTeam.Team} ({prediction.score.awayGoals})");
+            }
+
+            return predictions;
         }
     }
 }
